804b86eb27d269a0473226bf367ae63a89b69f02
Galileo Guzman <guzgali@gmail.com>
2013-07-11 01:47:14 -0500

    Portafolio HTML5 www.galileoguzman.com
    Portafolio propio hecho en HTML5 de manera interactiva.

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="Es">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+	<title>Galileo Guzmán</title>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="prefix.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	<header>
+		<h1>Galileo Guzmán<span><br/><a href="http://twitter.com/galileoguzman">@galileoguzman</a></span></h1>
+		<aside>
+			<ul>
+				<li><a href="http://facebook.com/galileoguzman"><img src="images/ico-facebook.png" alt="Facebook"></a></li>
+				<li><a href="http://twitter.com/galileoguzman"><img src="images/ico-twitter.png" alt="Twitter"></a></li>
+				<li><a href="#"><img src="images/ico-plus.png" alt="Plus"></a></li>
+				<li><a href="http://github.com/galileoguzman"><img src="images/ico-github.png" alt="Github"></a></li>
+			</ul>
+		</aside>
+	</header>
+	<section id="wrapper">
+		<section id="avatar">
+			<figure>
+				<img src="images/galileoguzman.jpg" alt="galileoguzman"/>
+			</figure>
+		</section>
+		<nav id="menu">
+			<ul>
+				<li>
+					<a href="#">
+						<h2>Habilidades y Conocimientos en TICs</h2>
+						<p>aprendidas a traves de 3 años de experiencia en el campo de la implementación de tecnologia de la información para la mejora de procesos de negocio.</p>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<h2>Actividades y Conocimientos</h2>
+						<p>adquiridas en el trancurso de los años algunos de manera autodidacta y otra tomando cursos de actualización por mi cuenta.</p>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<h2>Experiencia Laboral</h2>
+						<p>desarrollando e implementando proyectos/ideas dentro de organizaciones con presencia en internet.</p>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<h2>Lo que digo YO</h2>
+						<p>dentro de mi blog, donde abordo temas de internet y tecnologia.</p>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<!--<section id="info">
+
+		</section>-->
+	</section>
+	<footer>
+		
+	</footer>
+</body>
+</html>
